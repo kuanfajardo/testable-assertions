461316521a034d18a79939f1166745d3d14cf9d2
Juan Diego Fajardo <kangarookuan@gmail.com>
2021-06-11 16:51:46 -0700

    Add package manifest for Swift 5.5 in order to not retain a non-beta support. Additionally, bump min supported Swift version down to 5.3 again, to support all Xcode 12 builds

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,3 +22,4 @@ let package = Package(
       dependencies: ["TestableAssertions"]),
   ]
 )
+
